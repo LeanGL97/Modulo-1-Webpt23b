93ade108aef693032fd1de83e0f774b795edb5fb
Leandro Miguel Gianelli Louis <lean.louis1997@gmail.com>
2024-09-17 12:09:49 -0300

    Realicé la actividad de 'Testing'.Cree nuevos archivos para tener mas ordenado mi codigo js. Modificamos el archivo Html, agregando las nuevas rutas js.

---
+++ scripts/modelos.js
@@ -0,0 +1,35 @@
+class Activity {
+    constructor(id, title, description, imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+}
+
+class Repository {
+    constructor() {
+        this.activities = [];
+        this.id = 1;
+    }
+    
+    getAllActivities() {
+        return this.activities;
+    } 
+
+    createActivity(title, description, imgUrl) {
+        const activityId = this.id;
+        const newActivity = new Activity(activityId, title, description, imgUrl);
+        this.activities.push(newActivity);
+        this.id++;
+    }
+
+    deleteActivity(id){
+        this.activities = this.activities.filter(activity => activity.id !== id);
+    }
+}
+
+module.exports = {
+    Activity,
+    Repository,
+}
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,38 @@
-describe("demo", function () {
-  it("Este test debe pasar siempre", function () {
-    expect(4 + 2).toBe(6);
+// describe("demo", function () {
+//   it("Este test debe pasar siempre", function () {
+//     expect(4 + 2).toBe(6);
+//   });
+// });
+
+const { Activity, Repository } = require("../scripts/modelos");
+
+describe("Repository", () => {
+  const newRepository = new Repository();
+
+  it("Repository debería ser una clase", () => {
+    expect (typeof Repository).toBe("function");
   });
-});
+
+  it("Debería ser una instancia de la clase repository", () => {
+    expect (newRepository instanceof Repository).toBe(true);
+  });
+
+  it("Debería tener un método llamado getAllActivities", () => {
+    expect (newRepository.getAllActivities).toBeDefined;
+  });
+
+  it("Debería tener un método llamado createActivity", () => {
+    expect (newRepository.createActivity).toBeDefined;
+  });
+
+  it("Debería tener un método llamado deleteActivity", () => {
+    expect (newRepository.deleteActivity).toBeDefined;
+  });
+
+  it("El método createActivity debería poder agregar un elemento a la lista", () => {
+    newRepository.createActivity("title test", "description test", "www.urltest.com");
+
+    expect (newRepository.getAllActivities().length).toBe(1);
+  });
+
+})
